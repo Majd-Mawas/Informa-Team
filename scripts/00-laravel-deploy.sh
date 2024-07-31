d0a26bd337dccf5479926dbd6da2496885e3e172
unknown <majd.mawas@raizer.tech>
2024-07-31 22:13:54 +0300

    minor docker changes

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Update Composer to version 2 if not already installed
+echo "Updating Composer..."
+composer self-update --2
+
 # Ensure Composer dependencies are installed
 echo "Running composer"
 composer global require hirak/prestissimo
